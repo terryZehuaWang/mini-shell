56f9fc3c5dac13e05280464cb2182c4c3a8b6235
terryZehuaWang <terrywangze@gmail.com>
2026-01-21 16:06:03 +0000

    Add REPL and tokenizer

---
+++ Makefile
@@ -0,0 +1,5 @@
+CXX = g++
+CXXFLAGS = -Wall -Wextra -Werror -g -O0 -std=c++17
+
+minishell: src/main.cpp
+	$(CXX) $(CXXFLAGS) $^ -o $@
---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+std::vector<std::string> tokenize(const std::string& line);
+void push_current_to_tokens_logic(
+    std::string& current,
+    std::vector<std::string>& tokens);  // tokenize helper
+
+void print_tokens(std::vector<std::string> const& tokens);
+
+int main() {
+  std::string line;
+  std::vector<std::string> tokens;
+  while (true) {
+    std::cout << "minishell$ " << std::flush;
+    if (!std::getline(std::cin, line)) {
+      std::cout << "\nEOF\n";
+      break;
+    }
+
+    tokens = tokenize(line);
+    print_tokens(tokens);
+  }
+  return 0;
+}
+
+std::vector<std::string> tokenize(const std::string& line) {
+  std::vector<std::string> tokens;
+  std::string current;
+  for (size_t i = 0; i < line.size(); i++) {
+    char c = line[i];
+    if (c == ' ') {
+      push_current_to_tokens_logic(current, tokens);
+    } else if (c == '>' && i + 1 < line.size() && line[i + 1] == '>') {
+      push_current_to_tokens_logic(current, tokens);
+      tokens.push_back(">>");
+      i++;
+    } else if (c == '|' || c == '<' || c == '>' || c == '&') {
+      push_current_to_tokens_logic(current, tokens);
+      tokens.push_back(std::string(1, c));
+    } else {
+      current += c;
+    }
+  }
+  if (!current.empty()) {
+    tokens.push_back(current);
+  }
+  return tokens;
+}
+
+void push_current_to_tokens_logic(std::string& current,
+                                  std::vector<std::string>& tokens) {
+  if (!current.empty()) {
+    tokens.push_back(current);
+    current.clear();
+  }
+}
+
+void print_tokens(std::vector<std::string> const& tokens) {
+  std::cout << "tokens:"<<std::endl;
+  for (size_t i = 0; i < tokens.size(); ++i) 
+    std::cout<<tokens[i]<<std::endl;
+}
