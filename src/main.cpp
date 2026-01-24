d38b9c6ffb559fbaf1cb92decda6456af0f509aa
terryZehuaWang <terrywangze@gmail.com>
2026-01-24 19:34:13 +0000

    Add & operator parse

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,15 @@ void push_current_to_tokens_logic(
     std::vector<std::string>& tokens);  // tokenize helper
 
 void print_tokens(std::vector<std::string> const& tokens);
-void try_external_command(std::vector<std::string> const& tokens);
+void try_external_command(std::vector<std::string> const& tokens,
+                          bool const& is_foreground);
 
 bool try_builtin(std::vector<std::string> const& tokens);
-bool try_external_command_with_directions(
-    std::vector<std::string> const& tokens);
+bool try_external_command_with_redirections(
+    std::vector<std::string> const& tokens, bool const& is_foreground);
 std::vector<char*> convert_std_strings_to_c_strings(
     std::vector<std::string> const& std_strings);
-void parent_wait(int const& pid);
+void foreground_wait(int const& pid);
 bool parse_redirections(std::vector<std::string> const& tokens,
                         std::vector<std::string>& arguments,
                         std::string& target_file);
@@ -30,6 +31,7 @@ void exit_on_open_error(int fd);
 void exit_on_dup2_error(int dup2_return);
 int main() {
   std::string line;
+  bool is_foreground;
   while (true) {
     std::cout << "minishell$ " << std::flush;
     if (!std::getline(std::cin, line)) {
@@ -40,93 +42,19 @@ int main() {
     if (try_builtin(tokens)) {
       continue;
     }
-    if (try_external_command_with_directions(tokens) == true) continue;
-    try_external_command(tokens);
+
+    if (tokens[tokens.size()] == "&") {
+      tokens.pop_back();
+      is_foreground = false;
+    } else {
+      is_foreground = true;
+    }
+    if (try_external_command_with_redirections(tokens, is_foreground)) continue;
+    try_external_command(tokens, is_foreground);
   }
   return 0;
 }
-bool parse_redirections(std::vector<std::string> const& tokens,
-                        std::vector<std::string>& token_arguments,
-                        std::string& direction_str, std::string& target_file) {
-  token_arguments.clear();
-  bool target_file_exist = false;
-  for (size_t i = 0; i < tokens.size(); i++) {
-    if (tokens[i] == "<" || tokens[i] == ">" || tokens[i] == ">>") {
-      if (i + 1 >= tokens.size()) {
-        std::cerr << "syntax error: expected file after <<tokens[i]"
-                  << std::endl;
-        return false;
-      }
-      direction_str = tokens[i];
-      target_file = tokens[i + 1];
-      target_file_exist = true;
-      i++;
-    } else
-      token_arguments.push_back(tokens[i]);
-  }
 
-  return (target_file_exist);
-}
-bool try_external_command_with_directions(
-    std::vector<std::string> const& tokens) {
-  std::vector<std::string> token_arguments;
-  std::string direction_str;
-  std::string target_file;
-  struct direction_const {
-    int const direction_out = 0;
-    int const direction_in = 1;
-    int const direction_in_append = 2;
-  };
-  if (!parse_redirections(tokens, token_arguments, direction_str, target_file))
-    return false;
-
-  std::vector<char*> arguments =
-      convert_std_strings_to_c_strings(token_arguments);
-  arguments.push_back(nullptr);
-
-  int pid = fork();
-  if (pid == 0) {
-    //< in
-    //> out
-    //>> out append
-    int open_flag;
-    if (direction_str == "<")
-      open_flag = O_RDONLY;
-    else if (direction_str == ">")
-      open_flag = O_WRONLY | O_TRUNC | O_CREAT;
-    else if (direction_str == ">>")
-      open_flag = O_WRONLY | O_APPEND | O_CREAT;
-
-    int fd = open(target_file.c_str(), open_flag);
-    exit_on_open_error(fd);
-    exit_on_dup2_error(
-        dup2(fd, (direction_str == "<") ? STDIN_FILENO : STDOUT_FILENO));
-    close(fd);
-    execvp(arguments[0], arguments.data());
-    std::cerr << "execvp failed. Error message: " << std::strerror(errno)
-              << std::endl;
-
-  } else if (pid == -1) {
-    std::cerr << "fork failed. Error message: " << std::strerror(errno)
-              << std::endl;
-    return true;  // to avoid printing fork error message again in try external
-                  // command
-  }
-  parent_wait(pid);
-  return true;
-}
-void exit_on_open_error(int fd) {
-  if (fd < 0) {
-    std::cerr << "open failed: " << std::strerror(errno) << "\n";
-    _exit(1);
-  }
-}
-void exit_on_dup2_error(int dup2_return) {
-  if (dup2_return < 0) {
-    std::cerr << "dup2 failed: " << std::strerror(errno) << "\n";
-    _exit(1);
-  }
-}
 std::vector<std::string> tokenize(const std::string& line) {
   std::vector<std::string> tokens;
   std::string current;
@@ -165,7 +93,8 @@ void print_tokens(std::vector<std::string> const& tokens) {
     std::cout << tokens[i] << std::endl;
 }
 
-void try_external_command(std::vector<std::string> const& tokens) {
+void try_external_command(std::vector<std::string> const& tokens,
+                          bool const& is_foreground) {
   char* command = const_cast<char*>(tokens[0].c_str());
   std::vector<char*> arguments = convert_std_strings_to_c_strings(tokens);
   arguments.push_back(nullptr);
@@ -175,8 +104,8 @@ void try_external_command(std::vector<std::string> const& tokens) {
     std::cerr << "execvp failed. Error message: " << std::strerror(errno)
               << std::endl;
     _exit(127);
-  } else if (pid > 0) {
-    parent_wait(pid);
+  } else if (pid > 0 && is_foreground) {
+    foreground_wait(pid);
   } else if (pid == -1) {
     std::cerr << "fork failed. Error message: " << std::strerror(errno)
               << std::endl;
@@ -192,7 +121,7 @@ std::vector<char*> convert_std_strings_to_c_strings(
   return c_strings;
 }
 
-void parent_wait(int const& pid) {
+void foreground_wait(int const& pid) {
   int child_status;
   while (true) {
     pid_t waited_pid = waitpid(pid, &child_status, 0);
@@ -209,4 +138,84 @@ bool try_builtin(std::vector<std::string> const& tokens) {
   }
   if (tokens[0] == "exit") std::exit(0);
   return false;
+}
+
+bool parse_redirections(std::vector<std::string> const& tokens,
+                        std::vector<std::string>& token_arguments,
+                        std::string& direction_str, std::string& target_file) {
+  token_arguments.clear();
+  bool target_file_exist = false;
+  for (size_t i = 0; i < tokens.size(); i++) {
+    if (tokens[i] == "<" || tokens[i] == ">" || tokens[i] == ">>") {
+      if (i + 1 >= tokens.size()) {
+        std::cerr << "syntax error: expected file after <<tokens[i]"
+                  << std::endl;
+        return false;
+      }
+      direction_str = tokens[i];
+      target_file = tokens[i + 1];
+      target_file_exist = true;
+      i++;
+    } else
+      token_arguments.push_back(tokens[i]);
+  }
+
+  return (target_file_exist);
+}
+bool try_external_command_with_redirections(
+    std::vector<std::string> const& tokens, bool const& is_foreground) {
+  std::vector<std::string> token_arguments;
+  std::string direction_str;
+  std::string target_file;
+  if (!parse_redirections(tokens, token_arguments, direction_str, target_file))
+    return false;
+
+  std::vector<char*> arguments =
+      convert_std_strings_to_c_strings(token_arguments);
+  arguments.push_back(nullptr);
+
+  int pid = fork();
+  if (pid == 0) {
+    //< in
+    //> out
+    //>> out append
+    int open_flag;
+    if (direction_str == "<")
+      open_flag = O_RDONLY;
+    else if (direction_str == ">")
+      open_flag = O_WRONLY | O_TRUNC | O_CREAT;
+    else if (direction_str == ">>")
+      open_flag = O_WRONLY | O_APPEND | O_CREAT;
+
+    int fd = open(target_file.c_str(), open_flag);
+    exit_on_open_error(fd);
+    exit_on_dup2_error(
+        dup2(fd, (direction_str == "<") ? STDIN_FILENO : STDOUT_FILENO));
+    close(fd);
+    execvp(arguments[0], arguments.data());
+    std::cerr << "execvp failed. Error message: " << std::strerror(errno)
+              << std::endl;
+
+  } else if (pid == -1) {
+    std::cerr << "fork failed. Error message: " << std::strerror(errno)
+              << std::endl;
+    return true;  // to avoid printing fork error message again in try external
+                  // command
+  }
+  if (is_foreground) {
+    foreground_wait(pid);
+  }
+  return true;
+}
+void exit_on_open_error(int fd) {
+  if (fd < 0) {
+    std::cerr << "open failed: " << std::strerror(errno) << "\n";
+    _exit(1);
+  }
+}
+void exit_on_dup2_error(int dup2_return) {
+  if (dup2_return < 0) {
+    std::cerr << "dup2 failed: " << std::strerror(errno) << "\n";
+    _exit(1);
+  }
 }
