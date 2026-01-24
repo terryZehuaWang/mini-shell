b837d83d555680a0caddf35c0f5897e41361d0ba
terryZehuaWang <terrywangze@gmail.com>
2026-01-24 04:41:34 +0000

    Add I/O redirections

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include <algorithm>
 #include <cerrno>
 #include <cstring>
 #include <iostream>
@@ -12,11 +15,19 @@ void push_current_to_tokens_logic(
     std::vector<std::string>& tokens);  // tokenize helper
 
 void print_tokens(std::vector<std::string> const& tokens);
-void run_external_command(std::vector<std::string> const& tokens);
+void try_external_command(std::vector<std::string> const& tokens);
+
+bool try_builtin(std::vector<std::string> const& tokens);
+bool try_external_command_with_directions(
+    std::vector<std::string> const& tokens);
 std::vector<char*> convert_std_strings_to_c_strings(
     std::vector<std::string> const& std_strings);
-
-bool handle_builtin(std::vector<std::string> const& tokens);
+void parent_wait(int const& pid);
+bool parse_redirections(std::vector<std::string> const& tokens,
+                        std::vector<std::string>& arguments,
+                        std::string& target_file);
+void exit_on_open_error(int fd);
+void exit_on_dup2_error(int dup2_return);
 int main() {
   std::string line;
   while (true) {
@@ -26,14 +37,96 @@ int main() {
       break;
     }
     std::vector<std::string> tokens = tokenize(line);
-    if (handle_builtin(tokens)) {
+    if (try_builtin(tokens)) {
       continue;
     }
-    run_external_command(tokens);
+    if (try_external_command_with_directions(tokens) == true) continue;
+    try_external_command(tokens);
   }
   return 0;
 }
+bool parse_redirections(std::vector<std::string> const& tokens,
+                        std::vector<std::string>& token_arguments,
+                        std::string& direction_str, std::string& target_file) {
+  token_arguments.clear();
+  bool target_file_exist = false;
+  for (size_t i = 0; i < tokens.size(); i++) {
+    if (tokens[i] == "<" || tokens[i] == ">" || tokens[i] == ">>") {
+      if (i + 1 >= tokens.size()) {
+        std::cerr << "syntax error: expected file after <<tokens[i]"
+                  << std::endl;
+        return false;
+      }
+      direction_str = tokens[i];
+      target_file = tokens[i + 1];
+      target_file_exist = true;
+      i++;
+    } else
+      token_arguments.push_back(tokens[i]);
+  }
 
+  return (target_file_exist);
+}
+bool try_external_command_with_directions(
+    std::vector<std::string> const& tokens) {
+  std::vector<std::string> token_arguments;
+  std::string direction_str;
+  std::string target_file;
+  struct direction_const {
+    int const direction_out = 0;
+    int const direction_in = 1;
+    int const direction_in_append = 2;
+  };
+  if (!parse_redirections(tokens, token_arguments, direction_str, target_file))
+    return false;
+
+  std::vector<char*> arguments =
+      convert_std_strings_to_c_strings(token_arguments);
+  arguments.push_back(nullptr);
+
+  int pid = fork();
+  if (pid == 0) {
+    //< in
+    //> out
+    //>> out append
+    int open_flag;
+    if (direction_str == "<")
+      open_flag = O_RDONLY;
+    else if (direction_str == ">")
+      open_flag = O_WRONLY | O_TRUNC | O_CREAT;
+    else if (direction_str == ">>")
+      open_flag = O_WRONLY | O_APPEND | O_CREAT;
+
+    int fd = open(target_file.c_str(), open_flag);
+    exit_on_open_error(fd);
+    exit_on_dup2_error(
+        dup2(fd, (direction_str == "<") ? STDIN_FILENO : STDOUT_FILENO));
+    close(fd);
+    execvp(arguments[0], arguments.data());
+    std::cerr << "execvp failed. Error message: " << std::strerror(errno)
+              << std::endl;
+
+  } else if (pid == -1) {
+    std::cerr << "fork failed. Error message: " << std::strerror(errno)
+              << std::endl;
+    return true;  // to avoid printing fork error message again in try external
+                  // command
+  }
+  parent_wait(pid);
+  return true;
+}
+void exit_on_open_error(int fd) {
+  if (fd < 0) {
+    std::cerr << "open failed: " << std::strerror(errno) << "\n";
+    _exit(1);
+  }
+}
+void exit_on_dup2_error(int dup2_return) {
+  if (dup2_return < 0) {
+    std::cerr << "dup2 failed: " << std::strerror(errno) << "\n";
+    _exit(1);
+  }
+}
 std::vector<std::string> tokenize(const std::string& line) {
   std::vector<std::string> tokens;
   std::string current;
@@ -72,25 +165,21 @@ void print_tokens(std::vector<std::string> const& tokens) {
     std::cout << tokens[i] << std::endl;
 }
 
-void run_external_command(std::vector<std::string> const& tokens) {
+void try_external_command(std::vector<std::string> const& tokens) {
   char* command = const_cast<char*>(tokens[0].c_str());
   std::vector<char*> arguments = convert_std_strings_to_c_strings(tokens);
   arguments.push_back(nullptr);
   pid_t pid = fork();
   if (pid == 0) {
     execvp(command, arguments.data());
-    std::cerr << "execvp failed. Error message: " << std::strerror(errno) << std::endl;
+    std::cerr << "execvp failed. Error message: " << std::strerror(errno)
+              << std::endl;
     _exit(127);
   } else if (pid > 0) {
-    int child_status;
-    while (true) {
-      pid_t waited_pid = waitpid(pid, &child_status, 0);
-      if (waited_pid == -1 && errno == EINTR)  // interrupted by signal
-        continue;
-      break;
-    }
+    parent_wait(pid);
   } else if (pid == -1) {
-    std::cerr << "fork failed. Error message: "  << std::strerror(errno) << std::endl;
+    std::cerr << "fork failed. Error message: " << std::strerror(errno)
+              << std::endl;
   }
 }
 
@@ -103,7 +192,17 @@ std::vector<char*> convert_std_strings_to_c_strings(
   return c_strings;
 }
 
-bool handle_builtin(std::vector<std::string> const& tokens) {
+void parent_wait(int const& pid) {
+  int child_status;
+  while (true) {
+    pid_t waited_pid = waitpid(pid, &child_status, 0);
+    if (waited_pid == -1 && errno == EINTR)  // interrupted by signal
+      continue;
+    break;
+  }
+}
+
+bool try_builtin(std::vector<std::string> const& tokens) {
   if (tokens[0] == "cd") {
     if (chdir(tokens[1].c_str()) != 0) perror("cd");
     return true;
